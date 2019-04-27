d0322de76c0f0cf2eeaf59b4664e2ba801e42c54
Anetta <gnanetta@gmail.com>
2019-04-27 19:50:48 +0200

    environment created

---
+++ garden/GardenEnv.java
@@ -0,0 +1,33 @@
+// Environment code for project garden.mas2j
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import java.util.logging.*;
+
+public class GardenEnv extends Environment {
+
+    private Logger logger = Logger.getLogger("garden.mas2j."+GardenEnv.class.getName());
+
+    /** Called before the MAS execution with the args informed in .mas2j */
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        addPercept(ASSyntax.parseLiteral("percept(demo)"));
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+        logger.info("executing: "+action+", but not implemented!");
+        if (true) { // you may improve this condition
+             informAgsEnvironmentChanged();
+        }
+        return true; // the action was executed with success
+    }
+
+    /** Called before the end of MAS execution */
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
+
